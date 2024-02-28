7858be852b24ea55f59d2286776467fcc5c8a610
Jalaj <jalajsc@gmail.com>
2024-02-28 06:01:00 +0530

    commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
